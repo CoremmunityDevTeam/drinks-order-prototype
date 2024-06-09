d0fb3b3e67959700b677a801859f31f3fd1a0e18
Daniel Herbst <2283180+cht47@users.noreply.github.com>
2024-06-09 21:49:38 +0200

    Kompletter Rework der Startseite aufräumen vom CSS File, noch nicht fertig Beginn von Code Refactoring

--- public/index-script.js
+++ public/index-script.js
@@ -1,64 +1,100 @@
-// Load stored name on page load
-window.onload = async function() {
-    const response = await fetch('/api/get-username');
-    const data = await response.json();
-    if (data.username) {
-        document.getElementById('welcomeMessage').innerHTML = `Willkommen, ${data.username}`;
-        document.getElementById('loginButton').style.display = 'none';
-        document.getElementById('orderButton').style.display = 'block';
-        document.getElementById('logoutButton').style.display = 'block';
-        document.getElementById('adminButton').style.display = 'block';
-    } else {
-        document.getElementById('orderButton').style.display = 'none';
+document.addEventListener('DOMContentLoaded', function() {
+    async function loadUsername() {
+        const response = await fetch('/api/get-username');
+        const data = await response.json();
+        if (data.username) {
+            document.getElementById('welcomeMessage').innerHTML = `Willkommen, ${data.username}`;
+            document.getElementById('loginButton').style.display = 'none';
+            document.getElementById('orderButton').style.display = 'block';
+            document.getElementById('logoutButton').style.display = 'block';
+            document.getElementById('adminButton').style.display = 'block';
+        } else {
+            document.getElementById('welcomeMessage').innerHTML = '';
+            document.getElementById('loginButton').style.display = 'block';
+            document.getElementById('orderButton').style.display = 'none';
+            document.getElementById('logoutButton').style.display = 'none';
+            document.getElementById('adminButton').style.display = 'none';
+        }
     }
-};
 
-// Events laden
-async function loadEvents() {
-    const response = await fetch('/api/events');
-    const events = await response.json();
-    const eventList = document.getElementById('eventList');
-    eventList.innerHTML = '';
+    loadUsername();
 
-    // Group events by day
-    const groupedEvents = events.reduce((acc, event) => {
-        const eventDate = new Date(Date.parse(event.start_time));
-        const day = eventDate.toLocaleDateString('de-DE', { weekday: 'long' });
-        if (!acc[day]) acc[day] = [];
-        acc[day].push(event);
-        return acc;
-    }, {});
+    async function loadEvents() {
+        const response = await fetch('/api/events');
+        const events = await response.json();
+        const eventList = document.getElementById('eventList');
+        eventList.innerHTML = '';
 
-    // Add events
-    Object.keys(groupedEvents).forEach(day => {
-        const dayHeader = document.createElement('div');
-        dayHeader.classList.add('timeline-header');
-        dayHeader.innerHTML = `<span class="tag is-medium is-primary">${day}</span>`;
-        eventList.appendChild(dayHeader);
+        const groupedEvents = events.reduce((acc, event) => {
+            const eventDate = new Date(Date.parse(event.start_time));
+            const day = eventDate.toLocaleDateString('de-DE', { weekday: 'long' });
+            if (!acc[day]) acc[day] = [];
+            acc[day].push(event);
+            return acc;
+        }, {});
 
-        groupedEvents[day].forEach(event => {
-            const startTime = new Date(Date.parse(event.start_time));
-            const endTime = new Date(Date.parse(event.end_time));
+        Object.keys(groupedEvents).forEach(day => {
+            const dayHeader = document.createElement('div');
+            dayHeader.classList.add('timeline-header');
+            dayHeader.innerHTML = `<span class="tag is-medium is-primary">${day}</span>`;
+            eventList.appendChild(dayHeader);
 
-            const li = document.createElement('li');
-            li.classList.add('timeline-item');
-            li.innerHTML = `
-                <div class="timeline-marker"></div>
-                <div class="timeline-content">
-                    <p class="heading">${startTime.toLocaleTimeString('de-DE', { hour: '2-digit', minute: '2-digit' })} - ${endTime.toLocaleTimeString('de-DE', { hour: '2-digit', minute: '2-digit' })}</p>
-                    <p>${event.title}</p>
-                </div>
-            `;
-            eventList.appendChild(li);
+            groupedEvents[day].forEach(event => {
+                const startTime = new Date(Date.parse(event.start_time));
+                const endTime = new Date(Date.parse(event.end_time));
+
+                const li = document.createElement('li');
+                li.classList.add('timeline-item');
+                li.innerHTML = `
+                    <div class="timeline-marker"></div>
+                    <div class="timeline-content">
+                        <p class="heading">${startTime.toLocaleTimeString('de-DE', { hour: '2-digit', minute: '2-digit' })} - ${endTime.toLocaleTimeString('de-DE', { hour: '2-digit', minute: '2-digit' })}</p>
+                        <p>${event.title}</p>
+                    </div>
+                `;
+                eventList.appendChild(li);
+            });
         });
+
+        const endLi = document.createElement('li');
+        endLi.classList.add('timeline-header');
+        endLi.innerHTML = '<span class="tag is-medium is-primary">Ende</span>';
+        eventList.appendChild(endLi);
+    }
+
+    function showSection(title, sectionId) {
+        document.getElementById('sectionTitle').innerText = title;
+        document.getElementById('eventsSection').style.display = 'none';
+        document.getElementById(sectionId).style.display = 'block';
+        document.getElementById('contentContainer').style.display = 'block';
+        document.querySelector('.background-container').classList.add('blurry-bg');
+        document.querySelector('.menu-container').style.display = 'none';
+        document.getElementById('backButton').style.display = 'block';
+    }
+
+    function hideSection() {
+        document.getElementById('contentContainer').style.display = 'none';
+        document.querySelector('.background-container').classList.remove('blurry-bg');
+        document.querySelector('.menu-container').style.display = 'flex';
+        document.getElementById('backButton').style.display = 'none';
+        loadUsername(); // Benutzerinformationen neu laden
+    }
+
+    document.getElementById('menuEventsButton').addEventListener('click', function() {
+        if (document.getElementById('contentContainer').style.display === 'none') {
+            showSection('Events', 'eventsSection');
+            loadEvents(); // Events laden, wenn der Events-Button geklickt wird
+            document.getElementById('menuEventsButton').innerText = 'Zurück';
+        } else {
+            hideSection();
+            document.getElementById('menuEventsButton').innerText = 'Events';
+        }
     });
 
-    // Add "Ende" marker
-    const endLi = document.createElement('li');
-    endLi.classList.add('timeline-header');
-    endLi.innerHTML = '<span class="tag is-medium is-primary">Ende</span>';
-    eventList.appendChild(endLi);
-}
+    document.getElementById('backButton').addEventListener('click', function() {
+        hideSection();
+        document.getElementById('menuEventsButton').innerText = 'Events';
+    });
 
-// Initiales Laden der Events
-loadEvents();
+    loadEvents(); // Initiales Laden der Events beim Starten der Seite
+});
