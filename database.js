7e55f7fd5800c5e75ee303c5bcb222bb69f06a15
TechLogic <nils.heyer@gmx.de>
2024-08-02 16:14:20 +0200

    Added logs

--- database.js
+++ database.js
@@ -3,6 +3,7 @@ const sqlite3 = require('sqlite3').verbose();
 
 // SQLite Verbindung
 const dbFile = path.resolve(__dirname, 'getraenke.db');
+console.log("Connect to DB: "+ dbFile);
 const db = new sqlite3.Database(dbFile);
 
 module.exports = db;
