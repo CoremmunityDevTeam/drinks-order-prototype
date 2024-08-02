744bd74aed912dbfcc0174825fb6ab0698001210
TechLogic <nils.heyer@gmx.de>
2024-08-02 16:12:35 +0200

    Fix loading of db file

--- database.js
+++ database.js
@@ -1,6 +1,8 @@
+const path = require('path')
 const sqlite3 = require('sqlite3').verbose();
 
 // SQLite Verbindung
-const db = new sqlite3.Database('getraenke.db');
+const dbFile = path.resolve(__dirname, 'getraenke.db');
+const db = new sqlite3.Database(dbFile);
 
 module.exports = db;
